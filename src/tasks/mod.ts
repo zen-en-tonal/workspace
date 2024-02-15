7cf19d83fa11307d2815095e72a40d6e5855ed39
Takeru KODAMA <zen.en.tonal@gmail.com>
2024-02-15 13:16:12 +0000

    [Add] `fn` the helper

--- src/tasks/mod.ts
+++ src/tasks/mod.ts
@@ -1,4 +1,4 @@
-export { ident } from "./utils.ts";
+export { fn, ident } from "./utils.ts";
 export { deserialize, regexp, replace } from "./text.ts";
 export { request, toJson, toText } from "./http.ts";
 export { pick, prism } from "./object.ts";
--- src/tasks/utils.ts
+++ src/tasks/utils.ts
@@ -1,3 +1,12 @@
 export const ident = {
   run: <T>(arg: T) => Promise.resolve(arg),
 };
+
+/**
+ * fn wraps a function into a Task.
+ * @param f
+ * @returns
+ */
+export const fn = <T, Q>(f: (arg: T) => Q) => ({
+  run: (a: T) => Promise.resolve(f(a)),
+});
