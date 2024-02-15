bf2aaf2a4929d8fb6ad9146ef9d9d48aac2738c3
Takeru KODAMA <zen.en.tonal@gmail.com>
2024-02-15 22:37:50 +0000

    [Add] zod to parse a request json safely

---
+++ cmd/deps.ts
@@ -0,0 +1,2 @@
+export { dynamic } from "../src/mod.ts";
+export * as zod from "https://deno.land/x/zod@v3.22.4/mod.ts";
--- cmd/serve.ts
+++ cmd/serve.ts
@@ -1,7 +1,13 @@
-import { dynamic } from "../src/mod.ts";
+import { dynamic, zod } from "./deps.ts";
+
+const scheme = zod.object({
+  src: zod.string().url(),
+  context: zod.record(zod.any()),
+});
 
 Deno.serve(async (req: Request) => {
-  const { src, context } = await req.json();
+  const json = await req.json();
+  const { src, context } = await scheme.parseAsync(json);
   const run = await dynamic.dispatch(src);
   const result = await run(context);
   return new Response(JSON.stringify(result));
