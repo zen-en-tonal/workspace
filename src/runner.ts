d4f8e780eff86dc9b97768c6a82390be008fa6f1
Takeru KODAMA <zen.en.tonal@gmail.com>
2024-02-21 20:05:10 +0900

    [Add] timestamp field into result

--- src/runner.ts
+++ src/runner.ts
@@ -1,23 +1,36 @@
 export type Function<T, Q> = (arg: T) => Promise<Q>;
 
+type Meta = { _timestamp: string };
 export type Result<T> =
-  | { ok: true; value: T }
-  | { ok: false; message: string; cause?: string };
+  & (
+    | { ok: true; value: T }
+    | { ok: false; message: string; cause?: string }
+  )
+  & Meta;
+
+const timestamp = (now?: number) => new Date(now ?? Date.now()).toISOString();
 
 export function run<T, Q>(
   func: Function<T, Q>,
+  now?: number,
 ): (context: T) => Promise<Result<Q>> {
   return async (context: T) => {
     try {
       return {
         ok: true,
         value: await func(context),
+        _timestamp: timestamp(now),
       };
     } catch (e) {
       if (e instanceof Error) {
-        return { ok: false, message: e.message, cause: e.cause as string };
+        return {
+          ok: false,
+          message: e.message,
+          cause: e.cause as string,
+          _timestamp: timestamp(now),
+        };
       }
-      return { ok: false, message: "unknown" };
+      return { ok: false, message: "unknown", _timestamp: timestamp(now) };
     }
   };
 }
--- test/fragile_test.ts
+++ test/fragile_test.ts
@@ -5,6 +5,11 @@ Deno.test("fragile func", async () => {
   const fragile_func = () => {
     throw new Error("error");
   };
-  const res = await run(fragile_func)(null);
-  assertEquals(res, { ok: false, message: "error", cause: undefined });
+  const res = await run(fragile_func, 1708087637000)(null);
+  assertEquals(res, {
+    ok: false,
+    message: "error",
+    cause: undefined,
+    _timestamp: "2024-02-16T12:47:17.000Z",
+  });
 });
